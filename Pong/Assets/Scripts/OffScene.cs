2d49373ca7de24383990b58be767be1e536115ee
gabrielrspupo <gabrielrspupo@gmail.com>
2019-01-07 23:04:55 -0200

    Alterações no fim de jogo

--- Pong/Assets/Scripts/OffScene.cs
+++ Pong/Assets/Scripts/OffScene.cs
@@ -3,16 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class OffScene : MonoBehaviour {
-
-    public GameObject ballPrefab;
     public Bumper goalOwner;
     public GameController gameController;
 	
 	void OnTriggerEnter (Collider other) {
         if (other.tag == "Ball") {
             Destroy(other.gameObject);
-            if (!gameController.gameOver)
-            	Instantiate(ballPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+            gameController.newRound();
             goalOwner.score++;
         }
 	}
